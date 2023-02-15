053f3dfce6fbf82511f7c505ca4c0be357f5f519
juice1000 <julienlook@gmx.de>
2023-02-16 00:29:45 +0100

    almost done with ui, additionally added most javascript functionality

--- src/main.js
+++ src/main.js
@@ -1,6 +1,75 @@
 
 console.log('test log - script running')
 
+let cards = [
+    {name: "Ace", value: 11},
+    {name: "Two", value: 2},
+    {name: "Three", value: 3},
+    {name: "Four", value: 4},
+    {name: "Five", value: 5},
+    {name: "Six", value: 6},
+    {name: "Seven", value: 7},
+    {name: "Eight", value: 8},
+    {name: "Nine", value: 9},
+    {name: "Ten", value: 10},
+    {name: "Jack", value: 10},
+    {name: "Queen", value: 10},
+    {name: "King", value: 10},
+]
+
+let infoCards = {
+    start: 'Choose "Hit" if you want to continue or "Stand" if you want to stop',
+    continue: "Are you brave enough to continue?",
+    switch: "Now it's the dealer's turn!",
+    failed:  "You lost!",
+    win: "YOU WIN!!!",
+    rematch: "Do you want a rematch?"
+}
+
+let dealerScore = 0;
+let playerScore = 0;
+
+// simplyfied because chances of one game having more than 4 times the same value are close to impossible
+function getRandomCard(max) {
+    // source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    return Math.floor(Math.random() * max);
+}
+
+function addCards(){
+    let newCard = cards[getRandomCard(12)]
+    console.log("Adding Card: ", newCard.name)
+    playerScore += newCard.value
+    console.log(playerScore)
+
+    // if card value > 21: fail 
+    if (playerScore > 21) {
+        // however if Ace was last card and player failed, Ace becomes "1", value will be corrected
+        if (newCard.value === "Ace" && playerScore - 10 < 21){
+            playerScore -= 10
+        }
+        else {
+            $('#info').text(infoCards.failed);
+            $('#hit').prop("disabled", true)
+            setTimeout(rematch_info, 2000);
+        }
+    }
+    $('#player').text(`Current Score: ` + playerScore);
+    $('#info').text(infoCards.continue)
+}
+
+function rematch_info() {
+    $('#rematch').show()
+    $('#info').text(infoCards.rematch)
+}
+
+function addCardsDealer(){
+    $('#info').text(infoCards.switch)
+    $('#hit').prop("disabled", true)
+    //setTimeout(dealer_info, 2000);
+    // while card value not >= player's card value
+    // if card value > 21: fail 
+    // if Ace was last card and player failed, Ace becomes "1", value will be corrected
+}
 
 $(document).ready(function() {
     $('#butt').click(function() {
@@ -10,4 +79,9 @@ $(document).ready(function() {
             $("#main").show()
         });
     });
+
+    $('#dealer').text(`Current Score: ` + dealerScore);
+    $('#player').text(`Current Score: ` + playerScore);
+    $('#info').text(infoCards.start);
+
 });

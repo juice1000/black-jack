c8a37c9acee8044f1ce8964a8f1287f8482e695c
juice1000 <julienlook@gmx.de>
2023-02-15 18:39:53 +0100

    introduced jQuery, made responsive event that switches shows/ hides elements

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
 
-console.log('running script')
+console.log('test log - script running')
+
+
+$(document).ready(function() {
+    $('#butt').click(function() {
+        console.log("intro button clicked")
+        $(this).hide("slow");
+        $("#intro").hide("slow", function(){
+            $("#main").show()
+        });
+    });
+});
